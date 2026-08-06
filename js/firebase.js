f8aa8748868002588361c3aab96a8f971f9086a3
mraka7852 <mraka7852@gmail.com>
2026-08-06 08:42:05 +0700

    Add firebase config

--- js/firebase.js
+++ js/firebase.js
@@ -10,17 +10,27 @@ import {
    GANTI dengan config project Firebase kamu sendiri
    (Firebase Console > Project settings > Your apps > Web app)
    ========================================================= */
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "GANTI_DENGAN_API_KEY",
-  authDomain: "GANTI.firebaseapp.com",
-  databaseURL: "https://GANTI-default-rtdb.firebaseio.com/",
-  projectId: "GANTI",
-  storageBucket: "GANTI.firebasestorage.app",
-  messagingSenderId: "GANTI",
-  appId: "GANTI"
+  apiKey: "AIzaSyCo_tUSBQHNuR12yGN9UZHl7cKK3RwQjQc",
+  authDomain: "annidhomiyah-web.firebaseapp.com",
+  projectId: "annidhomiyah-web",
+  storageBucket: "annidhomiyah-web.firebasestorage.app",
+  messagingSenderId: "214286537603",
+  appId: "1:214286537603:web:58401921a39e332a36dcb6",
+  measurementId: "G-9Z7ZXBDGR4"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 const db = getDatabase(app);
 const auth = getAuth(app);
 
